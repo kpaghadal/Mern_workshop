cdb29c61dc6edf6b87c4059af4a90d5af21a2bb1
kpaghadal <kpaghadal45@gmail.com>
2026-02-27 13:45:08 +0530

    Frontend added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import DBInteraction from "./dbinteraction1";
+function App() {
+
+
+  return (
+    <>
+      {/* <h1>Welcome to React</h1> */}
+      < DBInteraction/>
+    </>
+  )
+}
+
+export default App
